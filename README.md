da643d443bcaab7e7ea26f6e56862a418effed66
bcollinsNR <bcollins@bcollins-mbp.local>
2013-10-17 17:12:58 -0700

    Added localhost check

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ New Relic PHP Agent Diagnostic Tool
 - [x] RUM
 - [x] Request queuing
 - [ ] Show relevent phpinfo()
-- [ ] Full license key
 - [ ] Daemon processes  
 - [ ] Firewall
 - [ ] OS security
--- nrPHPdiag.php
+++ nrPHPdiag.php
@@ -2,8 +2,8 @@
 
 // ******************************** New Relic ********************************
 //
-// PHP Agent Diagnostic Tool v 0.3.3
-// Author: Tony Mayse, Brian Collins, Steven Minor
+// PHP Agent Diagnostic Tool v 0.4.2
+// Author: Brian Collins, Steven Minor
 //
 // ***************************************************************************
 //
@@ -20,106 +20,111 @@
 // Report all PHP errors
 error_reporting(E_ALL);
 
-// Get phpinfo() into array
-ob_start();
-phpinfo();
-$phpinfo = array('phpinfo' => array());
-if(preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER))
-  foreach($matches as $match)
-    if(strlen($match[1]))
-        $phpinfo[$match[1]] = array();
-      elseif(isset($match[3]))
-        $phpinfo[end(array_keys($phpinfo))][$match[2]] = isset($match[4]) ? array($match[3], $match[4]) : $match[3];
-      else
-        $phpinfo[end(array_keys($phpinfo))][] = $match[2];
+// Only run if localhost 
+if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
 
-// Begin output of diagnostic data
-print "<pre>";
+  // Get phpinfo() into array
+  ob_start();
+  phpinfo();
+  $phpinfo = array('phpinfo' => array());
+  if(preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER))
+    foreach($matches as $match)
+      if(strlen($match[1]))
+          $phpinfo[$match[1]] = array();
+        elseif(isset($match[3]))
+          $phpinfo[end(array_keys($phpinfo))][$match[2]] = isset($match[4]) ? array($match[3], $match[4]) : $match[3];
+        else
+          $phpinfo[end(array_keys($phpinfo))][] = $match[2];
 
-// Basic table of phpinfo() data 
-// TODO: This will be trimmed down considerably to just what we need
-echo '<table>';
-foreach ($phpinfo as $value1) {
-  foreach ($value1 as $key2 => $value2) {
-    if (is_array($value2)) {
-      foreach ($value2 as $key3 => $value3) {
-        echo '<tr><td>'.$key3.'</td><td>'.$value3.'</td></tr>';
+  // Begin output of diagnostic data
+  print "<pre>";
+
+  // Basic table of phpinfo() data 
+  // TODO: This will be trimmed down considerably to just what we need
+  echo '<table>';
+  foreach ($phpinfo as $value1) {
+    foreach ($value1 as $key2 => $value2) {
+      if (is_array($value2)) {
+        foreach ($value2 as $key3 => $value3) {
+          echo '<tr><td>'.$key3.'</td><td>'.$value3.'</td></tr>';
+        }
+      }  
+      else {
+        echo '<tr><td>'.$key2.'</td><td>'.$value2.'</td></tr>';
       }
-    }  
-    else {
-      echo '<tr><td>'.$key2.'</td><td>'.$value2.'</td></tr>';
     }
   }
-}
-echo '</table>';
+  echo '</table>';
 
-//print_r($phpinfo['phpinfo']); //don't display $phpinfo['phpinfo'][0] & $phpinfo['phpinfo'][1]
-//print_r($phpinfo['Apache Environment']);
-//print_r($phpinfo['PHP Variables']); //don't display $phpinfo['PHP Variables']['_SERVER["argv"]']
-//print_r($phpinfo['<newrelic>']); //what is this name set to? 
-//there may be a few more sections we need
+  //print_r($phpinfo['phpinfo']); //don't display $phpinfo['phpinfo'][0] & $phpinfo['phpinfo'][1]
+  //print_r($phpinfo['Apache Environment']);
+  //print_r($phpinfo['PHP Variables']); //don't display $phpinfo['PHP Variables']['_SERVER["argv"]']
+  //print_r($phpinfo['<newrelic>']); //what is this name set to? 
+  //there may be a few more sections we need
 
-$numberOfLines = '2'; //customer input? param could be simple way to implement this if this need to be customizable
-$tailCommand = 'tail -'.$numberOfLines;
+  $numberOfLines = '2'; //customer input? param could be simple way to implement this if this need to be customizable
+  $tailCommand = 'tail -'.$numberOfLines;
 
-//TODO: Get the actual locations of the logs from config, for now assume default
-//$agentLogLocation = //get from phpinfo() array
-//$daemonLogLocation = //get from phpinfo() array
-$defaultAgentLogLocation = '/var/log/newrelic/php_agent.log';
-$defaultDaemonLogLocation = '/var/log/newrelic/newrelic-daemon.log';
+  //TODO: Get the actual locations of the logs from config, for now assume default
+  //$agentLogLocation = //get from phpinfo() array
+  //$daemonLogLocation = //get from phpinfo() array
+  $defaultAgentLogLocation = '/var/log/newrelic/php_agent.log';
+  $defaultDaemonLogLocation = '/var/log/newrelic/newrelic-daemon.log';
 
-$agentLogLocation = $defaultAgentLogLocation;
-$daemonLogLocation = $defaultDaemonLogLocation;
-echo "<b>Agent Log (".$tailCommand.")</b>";
-echo "<br />";
-passthru($tailCommand.' '.$agentLogLocation);
-echo "<br />";echo "<br />";
-echo "<b>Daemon Log (".$tailCommand.")</b>";
+  $agentLogLocation = $defaultAgentLogLocation;
+  $daemonLogLocation = $defaultDaemonLogLocation;
+  echo "<b>Agent Log (".$tailCommand.")</b>";
+  echo "<br />";
+  passthru($tailCommand.' '.$agentLogLocation);
+  echo "<br />";echo "<br />";
+  echo "<b>Daemon Log (".$tailCommand.")</b>";
 
-echo "<br />";
-passthru($tailCommand.' '.$daemonLogLocation);
-// Basic RUM check
-echo "<br /><script>
-  if(typeof NREUMQ != 'undefined') {
-    document.write ('<b>RUM</b><br />');
-    document.write (NREUMQ);
-    document.write ('<br />');
-  }
-  else 
-    document.write ('<b>No RUM</b>');</script>";
-
+  echo "<br />";
+  passthru($tailCommand.' '.$daemonLogLocation);
+  // Basic RUM check
+  echo "<br /><script>
+    if(typeof NREUMQ != 'undefined') {
+      document.write ('<b>RUM</b><br />');
+      document.write (NREUMQ);
+      document.write ('<br />');
+    }
+    else 
+      document.write ('<b>No RUM</b>');</script>";
 
 
   /*
-From PHP test tools:
-<script>
-if (document.documentElement.innerHTML.search("NREUMQ=NREUMQ")) {
-  document.write("<b>Test Passed, found RUM header.</b><br>\n");
-} else {
-  document.write("<b>Test Failed, didn't find RUM header.</b><br>\n");
-}
+  // From PHP test tools: this checks for both header and footer.
+  <script>
+  if (document.documentElement.innerHTML.search("NREUMQ=NREUMQ")) {
+    document.write("<b>Test Passed, found RUM header.</b><br>\n");
+  } else {
+    document.write("<b>Test Failed, didn't find RUM header.</b><br>\n");
+  }
 
-if (document.documentElement.innerHTML.search("NREUMQ.f=function")) {
-  document.write("<b>Test Passed, found RUM footer.</b><br>\n");
-} else {
-  document.write("<b>Test Failed, didn't find RUM footer.</b><br>\n");
-}
-</script>
-
+  if (document.documentElement.innerHTML.search("NREUMQ.f=function")) {
+    document.write("<b>Test Passed, found RUM footer.</b><br>\n");
+  } else {
+    document.write("<b>Test Failed, didn't find RUM footer.</b><br>\n");
+  }
+  </script>
   */
 
-// Basic request queueing header check
-echo '<br /><br />';
-echo '<b>Request Queuing</b>';
-echo '<br />';
-if (array_key_exists('HTTP_X_REQUEST_START', $_SERVER))
+  // Basic request queueing header check
+  echo '<br /><br />';
+  echo '<b>Request Queuing</b>';
+  echo '<br />';
+  if (array_key_exists('HTTP_X_REQUEST_START', $_SERVER))
+  {
+    echo "Yes:";
+    echo $_SERVER["HTTP_X_REQUEST_START"];
+  }
+  else {
+    echo "No";
+  }
+  print "</pre>";
+}
+else
 {
-  echo "Yes:";
-  echo $_SERVER["HTTP_X_REQUEST_START"];
+  echo "Access denied: non-local request detected.";
 }
-else {
-  echo "No";
-}
-
-print "</pre>";
 ?>
